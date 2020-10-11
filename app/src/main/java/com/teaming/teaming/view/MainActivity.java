ee398fda53a83ca64f4762b4e114deb732a7e74c
Mackenzie <shonayimack@gmail.com>
2020-10-11 15:42:37 +0200

    Add 3 new sub-packages and moved Main Activity to the "view" package

--- app/src/main/java/com/teaming/teaming/view/MainActivity.java
+++ app/src/main/java/com/teaming/teaming/view/MainActivity.java
@@ -1,8 +1,10 @@
-package com.teaming.teaming;
+package com.teaming.teaming.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.teaming.teaming.R;
 
 public class MainActivity extends AppCompatActivity {
 
